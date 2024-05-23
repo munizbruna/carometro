964c175182bd0a6950224620f6ddb7f0f040d726
munizbruna <bompatricio@gmail.com>
2024-05-23 10:09:13 -0300

    controllers>turmas atualizada

--- controllers/turmas.js
+++ controllers/turmas.js
@@ -1,11 +1,15 @@
 const Turmas = require('../models/turmas');
+const Usuario = require('../models/usuarios')
+const UsuariosTurmas = require('../models/usuarios_turma')
 
+//mostra todas as turmas
 exports.getAll = async (req, res) => {
     const turmas = await Turmas.findAll();
     res.json(turmas)
 };
 
-exports.getById = async (req, res) => {
+//retorna uma turma pelo id
+/* exports.getById = async (req, res) => {
     try {
         const idDoParam = req.params.id;
 
@@ -17,8 +21,9 @@ exports.getById = async (req, res) => {
             turmaEncontrada = await Turmas.findOne({ where: { codigo: idDoParam } });
         }
 
-        // Verifica se encontrou a turma e responde de acordo
+        // Verifica se encontrou a turma e responde com os alunos dessa turma
         if (turmaEncontrada) {
+
             return res.json(turmaEncontrada);
         } else {
             return res.status(404).send('Não foi possível encontrar a turma.');
@@ -27,8 +32,43 @@ exports.getById = async (req, res) => {
         console.error("Erro ao buscar turma:", error);
         return res.status(500).send('Ocorreu um erro ao buscar a turma.');
     }
+}; */
+
+//retorna os alunos da turma
+exports.getById = async (req, res) => {
+    try {
+        const idDoParam = req.params.id;
+
+
+        console.log("idDoParam", idDoParam)
+        // Busca a turma pelo ID
+        let turmaEncontrada = await Turmas.findOne({ where: { idTurmas: idDoParam }/* , include: Usuario  */});
+
+        //turmaEncontrada.id
+        //...titulo
+        //turmaEncontrada.usuario: []
+
+
+        // Se não encontrar a turma pelo ID, busca pelo código
+        /*    if (!turmaEncontrada) {
+               turmaEncontrada = await Turmas.findOne({ where: { codigo: idDoParam } });
+           } */
+
+        // Verifica se encontrou a turma e responde de acordo
+        if (turmaEncontrada) {
+            return res.json(turmaEncontrada);
+        } else {
+            console.log("entrou aqui")
+            return res.status(404).send('Não foi possível encontrar a turma.');
+        }
+    } catch (error) {
+        console.error("Erro ao buscar turma:", error);
+        return res.status(500).send('Ocorreu um erro ao buscar a turma.');
+    }
 };
 
+
+//cria turma
 
 exports.createTurma = async (req, res) => {
     const turmaCadastrada = await Turmas.findOne({ where: { codigo: req.body.codigo } });
@@ -50,16 +90,11 @@ exports.updateTurma = async (req, res) => {
     try {
         const turmaCadastrada = await Turmas.findOne({ where: { codigo: codigoTurma } });
 
-        console.log("aqui", turmaCadastrada)
         if (turmaCadastrada) {
-
             delete req.body.codigo;
-
             const [numRowsUpdated] = await Turmas.update(req.body, {
                 where: { codigo: codigoTurma }
             });
-
-
             if (numRowsUpdated > 0) {
                 const turmaAtualizada = await Turmas.findOne({ where: { codigo: codigoTurma } });
                 return res.send({ message: 'Turma Atualizada com sucesso', turmacomdadosnovos: turmaAtualizada });
